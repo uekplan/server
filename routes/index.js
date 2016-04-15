7e0884df20de36dcd922ce7e0799d4dae35fe957
Przemysław Płyś <przemyslaw.plys@gmail.com>
2016-04-15 02:35:53 +0200

    Generated simple express project

---
+++ routes/index.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var router = express.Router();
+var Label = require('uekplan-models/label');
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'uekplan'});
+});
+
+/* GET home page. */
+router.get('/tutors', function (req, res, next) {
+
+    Label.find()
+        .where('type').equals('N')
+        .then((data)=> {
+            res.json(data);
+        }).catch((err)=> {
+        res.status(500).send();
+    })
+});
+/* GET home page. */
+router.get('/rooms', function (req, res, next) {
+
+    Label.find()
+        .where('type').equals('S')
+        .then((data)=> {
+            res.json(data);
+        }).catch((err)=> {
+        res.status(500).send();
+    })
+});
+/* GET home page. */
+router.get('/groups', function (req, res, next) {
+    Label.find()
+        .where('type').equals('G')
+        .then((data)=> {
+            res.json(data);
+        }).catch((err)=> {
+        res.status(500).send();
+    })
+});
+
+
+/* GET home page. */
+router.get('/buildings', function (req, res, next) {
+    Label.find()
+        .where('type').equals('B')
+        .then((data)=> {
+            res.json(data);
+        }).catch((err)=> {
+        res.status(500).send();
+    })
+});
+/* GET home page. */
+router.get('/fields', function (req, res, next) {
+    Label.find()
+        .where('type').equals('F')
+        .then((data)=> {
+            res.json(data);
+        }).catch((err)=> {
+        res.status(500).send();
+    })
+});
+
+
+module.exports = router;
